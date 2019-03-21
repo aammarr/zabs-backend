84e5d9bbf937a8f0c29f4a109d754e06818923cd
Ammar Ahmed <aammarr.ahmed@gmail.com>
2019-03-21 11:02:02 +0500

    change in notification

--- config/push-notification.php
+++ config/push-notification.php
@@ -2,12 +2,12 @@
 
 return array(
 
-    'madinati-app-ios' => array(
+    'zabs-app-ios' => array(
         'environment' 	=> 'development',
         'apiKey'  		=> 'AIzaSyBssgpJo_1lNCA7WYtC3EYwxUp9B59eLGs',
         'service'     	=> 'fcm'
     ),
-    'madinati-app-android' => array(
+    'zabs-app-android' => array(
         'environment' 	=> 'development',
         'apiKey'      	=> 'AIzaSyBssgpJo_1lNCA7WYtC3EYwxUp9B59eLGs',
         'service'     	=> 'fcm'
