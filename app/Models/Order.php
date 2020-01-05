74c7d0057620182ff8b59be2d9907f96f55c660c
ammar_fomark <ammar@fomarkmedia.com>
2020-01-05 21:10:44 +0500

    change in get order api

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,15 +14,27 @@ class Order extends Model
 
     public function getOrdersByUserId($user_id){
 
-    	$data = DB::table('orders as o')
+    	$orders = DB::table('orders as o')
+                ->leftJoin('vendor as v','v.id','o.vendor_id')
     			->where('o.deleted_at',null)
     			->where('o.user_id',$user_id)
     			->select(
-    				'o.*'
+    				'o.*',
+                    'v.name as vendor_name'
     			)
     			->orderBy('o.id','desc')
-    			->get();
+    			->get()->toArray();
 
-    	return $data;
+        $prodData=[];
+        foreach ($orders as $key => $value) {
+        
+            $value->products=[];
+            $value->products = DB::table('order_products')
+                            ->where('order_id',$value->id)
+                            ->select('product_name')
+                            ->get()->toArray();
+        }
+        
+    	return $orders;
     }
 }
